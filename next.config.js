cb708c1fd1f99392f086000467b5655ddada82d9
Andrea Casino <andrea.casino.2@gmail.com>
2020-01-29 21:31:57 +0000

    New deploy

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@ const path = require('path');
 const withSass = require('@zeit/next-sass');
 const withImages = require('next-images');
 
-const isProd = (process.env.NODE_ENV || 'production') === 'production'
-
 module.exports = withImages(withSass({
   webpack(config) {
     const aliases = {
@@ -20,5 +18,4 @@ module.exports = withImages(withSass({
     importLoaders: 1,
     localIdentName: '[local]-[hash:base64:5]',
   },
-  assetPrefix: isProd ? '/personal-page' : '',
 }));
